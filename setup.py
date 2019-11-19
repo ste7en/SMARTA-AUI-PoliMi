155646dff729298cc625c06e724e7800f2400469
Stefano Formicola <stefano_17sf@libero.it>
2019-11-19 18:43:58 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='smarta',
+    version='0.1',
+    author='Ferrara, Formicola, Mazzi, Palombini',
+    author_email='stefanoformicola@icloud.com',
+    description='',
+    url='',
+    packages=['smarta'],
+    install_requires=[],
+    scripts=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ]
+)
