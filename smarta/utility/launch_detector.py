3a1f46698614f3bc93413d442bdf40493b2470cb
Stefano Formicola <stefano_17sf@libero.it>
2019-12-11 23:57:18 +0100

    LaunchDetector skelethon (WIP)

--- smarta/utility/launch_detector.py
+++ smarta/utility/launch_detector.py
@@ -1,31 +1,21 @@
 from math import sqrt
+from statistics import mean
 from smarta.utility.accellerometer_manager import AccelerometerManager as Accelerometer
 import sched
+import threading
 import time
 
 
-class LaunchDetector:
-    __instance = None
-    __queue_length = 5
+class LaunchDetector(threading.Thread):
+    __queue_length = 15
     __acquisition_period_in_seconds = 0.1
 
     def __init__(self):
-        if LaunchDetector.__instance is None:
-            LaunchDetector.__instance = self
-            self.__vsa_array = [0.0, 0.0, 0.0, 0.0, 0.0]
-            self.__sched = sched.scheduler(time.time, time.sleep)
-            self.__accelerometer = Accelerometer.get_instance()
-        else:
-            raise Exception("This class is a Singleton")
-
-    @staticmethod
-    def get_instance():
-        """ Static access method. 
-        :rtype: LaunchDetector
-        """
-        if LaunchDetector.__instance is None:
-            LaunchDetector()
-        return LaunchDetector.__instance
+        super().__init__()
+        self.__vsa_array = []
+        self.__sched = sched.scheduler(time.time, time.sleep)
+        self.__accelerometer = Accelerometer.get_instance()
+        self.__stopped = False
 
     def _compute_vsa(self, x, y, z):
         """
@@ -44,17 +34,28 @@ class LaunchDetector:
         print('vsa_array = ', self.__vsa_array)
         # END TEST
 
-    def start(self):
+    def start_detection(self):
         # TODO: - Log this operation
-        self.__sched.enter(self.__acquisition_period_in_seconds, 1, self._sched_acquire_and_store_data)
+        self._enter_scheduler()
         self.__sched.run()
 
+    def run(self):
+        self.start_detection()
+
     def stop(self):
-        self.__vsa_array = [0.0, 0.0, 0.0, 0.0, 0.0]
+        self.__stopped = True
+        self.__vsa_array = []
 
     def _sched_acquire_and_store_data(self):
-        x = self.__accelerometer.get_accel_x() #get_gyro_x()
-        y = self.__accelerometer.get_accel_y() #get_gyro_y()
-        z = self.__accelerometer.get_accel_z() #get_gyro_z()
+        x = self.__accelerometer.get_accel_x()  # get_gyro_x()
+        y = self.__accelerometer.get_accel_y()  # get_gyro_y()
+        z = self.__accelerometer.get_accel_z()  # get_gyro_z()
         self._compute_vsa(x, y, z)
+        if not self.__stopped:
+            self._enter_scheduler()
+
+    def _enter_scheduler(self):
         self.__sched.enter(self.__acquisition_period_in_seconds, 1, self._sched_acquire_and_store_data)
+
+    def avg_acc_value(self):
+        return mean(self.__vsa_array)
