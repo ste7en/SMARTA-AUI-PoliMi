66cad223518f3d1c7b1c3306e9d1bed99c112d40
Stefano Formicola <stefano_17sf@libero.it>
2019-12-11 23:56:53 +0100

    AccelerometerManager refactor

--- smarta/utility/accellerometer_manager.py
+++ smarta/utility/accellerometer_manager.py
@@ -2,6 +2,10 @@ from smarta.utility.GPIOManager import GPIOManager
 
 
 class AccelerometerManager(GPIOManager):
+    """
+    Subclass of GPIOManager, responsible of managing data I/O with
+    the MPU6050, in particular data coming from the accelerometer.
+    """
     __PWR_M = 0x6B
     __DIV = 0x19
     __CONFIG = 0x1A
@@ -12,9 +16,6 @@ class AccelerometerManager(GPIOManager):
     __ACCEL_X = 0x3B
     __ACCEL_Y = 0x3D
     __ACCEL_Z = 0x3F
-    __GYRO_X = 0x43
-    __GYRO_Y = 0x45
-    __GYRO_Z = 0x47
 
     __DEV_ADDR = 0x68  # device address
 
@@ -37,7 +38,13 @@ class AccelerometerManager(GPIOManager):
             AccelerometerManager()
         return AccelerometerManager.__instance
 
-    def write(self, bus_address, value):
+    def write(self, bus_address, value) -> None:
+        """
+        Write operation on 8b register
+        :param bus_address: the address of the register
+        :param value: 8-bit value to write
+        :return: None
+        """
         super().write_byte_data(bus_address, value)
 
     def read(self, bus_address):
@@ -52,24 +59,47 @@ class AccelerometerManager(GPIOManager):
         else:
             return value
 
-    def __init_mpu(self):
+    def __init_mpu(self) -> None:
+        """
+        Initializes the MPU6050
+        :return: None
+        """
         self.write(self.__DIV, 7)
         self.write(self.__PWR_M, 1)
         self.write(self.__CONFIG, 0)
-        self.write(self.__ACC_CONFIG, 15)
-        self.write(self.__GYRO_CONFIG, 24)  # Full Scale Range of ± 4g
+        self.write(self.__ACC_CONFIG, 8)  # Full Scale Range of ± 4g
         self.write(self.__INT_EN, 1)
         super().gpio_sleep()
 
-    def get_accel_x(self):
+    def get_accel_x(self) -> float:
+        """
+        :return: x-axis value of acceleration
+        """
         return self.read(AccelerometerManager.__ACCEL_X)/8192.0  # Normalized to a Full Scale Range of ± 4g
 
-    def get_accel_y(self):
+    def get_accel_y(self) -> float:
+        """
+        :return: y-axis value of acceleration
+        """
         return self.read(AccelerometerManager.__ACCEL_Y)/8192.0  # Normalized to a Full Scale Range of ± 4g
 
-    def get_accel_z(self):
+    def get_accel_z(self) -> float:
+        """
+        :return: z-axis value of acceleration
+        """
         return self.read(AccelerometerManager.__ACCEL_Z)/8192.0  # Normalized to a Full Scale Range of ± 4g
 
+    """
+    These are the parameters to use in order to get data from the gyroscope:
+    
+    __GYRO_X = 0x43
+    __GYRO_Y = 0x45
+    __GYRO_Z = 0x47
+
+    Put the following line in the __init__():
+    self.write(self.__GYRO_CONFIG, 24)
+
+    Helper methods:
     def get_gyro_x(self):
         return self.read(AccelerometerManager.__GYRO_X)
 
@@ -77,4 +107,5 @@ class AccelerometerManager(GPIOManager):
         return self.read(AccelerometerManager.__GYRO_Y)
 
     def get_gyro_z(self):
-        return self.read(AccelerometerManager.__GYRO_Z)
+        return self.read(AccelerometerManager.__GYRO_Z) 
+    """
