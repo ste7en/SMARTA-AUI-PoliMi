c76eb103f0fb6ee4206a3a820066ac5844073bb5
Stefano Formicola <stefano_17sf@libero.it>
2019-12-12 15:55:28 +0100

    launch_detector refactored and documented

--- smarta/utility/launch_detector.py
+++ smarta/utility/launch_detector.py
@@ -17,7 +17,7 @@ class LaunchDetector(threading.Thread):
         self.__accelerometer = Accelerometer.get_instance()
         self.__stopped = False
 
-    def _compute_vsa(self, x, y, z):
+    def _compute_and_store_vsa(self, x, y, z) -> None:
         """
         Computes the vector sum of acceleration data and puts it into a __queue_length queue
         :param x: x-acceleration
@@ -34,7 +34,7 @@ class LaunchDetector(threading.Thread):
         print('vsa_array = ', self.__vsa_array)
         # END TEST
 
-    def start_detection(self):
+    def start_detection(self) -> None:
         # TODO: - Log this operation
         self._enter_scheduler()
         self.__sched.run()
@@ -42,20 +42,36 @@ class LaunchDetector(threading.Thread):
     def run(self):
         self.start_detection()
 
-    def stop(self):
+    def stop(self) -> None:
+        """
+        Stops the data acquisition and, consequently, the Thread
+        :return: None
+        """
         self.__stopped = True
         self.__vsa_array = []
 
-    def _sched_acquire_and_store_data(self):
+    def _sched_acquire_and_store_data(self) -> None:
+        """
+        Acquire accelerometer data, computes the VSA and stores it in the queue
+        :return: None
+        """
         x = self.__accelerometer.get_accel_x()  # get_gyro_x()
         y = self.__accelerometer.get_accel_y()  # get_gyro_y()
         z = self.__accelerometer.get_accel_z()  # get_gyro_z()
-        self._compute_vsa(x, y, z)
+        self._compute_and_store_vsa(x, y, z)
         if not self.__stopped:
             self._enter_scheduler()
 
-    def _enter_scheduler(self):
+    def _enter_scheduler(self) -> None:
+        """
+        Schedules the acquisition process after the specified '__acquisition_period_in_seconds' seconds
+        :return: None
+        """
         self.__sched.enter(self.__acquisition_period_in_seconds, 1, self._sched_acquire_and_store_data)
 
-    def avg_acc_value(self):
+    def avg_acc_value(self) -> float:
+        """
+        Computes the average value of the VSA queue
+        :return: average of the values stored in __vsa_array
+        """
         return mean(self.__vsa_array)
