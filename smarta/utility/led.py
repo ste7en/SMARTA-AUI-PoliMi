1660009c1368e779a278779597c2101f8fd2bb0a
Stefano Formicola <stefano_17sf@libero.it>
2019-12-13 12:19:05 +0100

    minor tab/spaces/comment fixes in led.py

--- smarta/utility/led.py
+++ smarta/utility/led.py
@@ -1,23 +1,22 @@
-from smarta.utility.GPIOManager import GPIOManager
 import time
 from rpi_ws281x import Color, PixelStrip, ws
 from threading import Thread
 from threading import Timer
 
 
-class LedManager():
+class LedManager(object):
 
     # LED blinking times configuration:
     __BLINKING_TIME_RED = 0.5
     __BLINKING_TIME_YELLOW = 1   # yellow light blinks more slowly
 
     # LED strip configuration:
-    __LED_COUNT = 3  # Number of LED pixels.
-    __LED_PIN = 18  # GPIO pin connected to the pixels (must support PWM!).
+    __LED_COUNT = 3         # Number of LED pixels.
+    __LED_PIN = 18          # GPIO pin connected to the pixels (must support PWM!).
     __LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-    __LED_DMA = 10  # DMA channel to use for generating signal (try 10)
+    __LED_DMA = 10          # DMA channel to use for generating signal (try 10)
     __LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-    __LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
+    __LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
     __LED_CHANNEL = 0
     __LED_STRIP = ws.WS2812_STRIP
 
@@ -26,48 +25,43 @@ class LedManager():
     def __init__(self):
         if LedManager.__instance is None:
             LedManager.__instance = self
-            self.strip = PixelStrip(self.__LED_COUNT, self.__LED_PIN, self.__LED_FREQ_HZ, self.__LED_DMA, self.__LED_INVERT,
-                               self.__LED_BRIGHTNESS, self.__LED_CHANNEL, self.__LED_STRIP)
+            self.strip = PixelStrip(self.__LED_COUNT, self.__LED_PIN, self.__LED_FREQ_HZ, self.__LED_DMA,
+                                    self.__LED_INVERT, self.__LED_BRIGHTNESS, self.__LED_CHANNEL, self.__LED_STRIP)
             self.strip.begin()
-
         else:
             raise Exception("This class is a Singleton")
 
     @staticmethod
     def get_instance():
         """
-
         :rtype: LedManager
         """
         if LedManager.__instance is None:
             LedManager()
         return LedManager.__instance
 
-
     # Define functions which animate LEDs in various ways.
-
     def __colorWipe(self, strip, color, wait_ms=50):
-
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, color)
             strip.show()
             time.sleep(wait_ms / 1000.0)
 
-    def __elenafunc(self):
-        """
-        Function to test LEDs
-        """
-        while True:
-            self.__colorWipe(self.strip, Color(255, 0, 0), 0)  # Red wipe
-            time.sleep(1)
-            self.__colorWipe(self.strip, Color(0, 255, 0), 0)  # Green wipe
-            time.sleep(1)
-            self.__colorWipe(self.strip, Color(0, 0, 255), 0)  # Blue wipe
-            time.sleep(1)
-            self.__colorWipe(self.strip, Color(255, 255, 255), 0)  # Composite White wipe
-            time.sleep(1)
-            self.__colorWipe(self.strip, Color(255, 255, 255, 255), 0)  # Composite White + White LED wipe
-            time.sleep(1)
+    # def __elenafunc(self):
+    #     """
+    #     Function to test LEDs
+    #     """
+    #     while True:
+    #         self.__colorWipe(self.strip, Color(255, 0, 0), 0)  # Red wipe
+    #         time.sleep(1)
+    #         self.__colorWipe(self.strip, Color(0, 255, 0), 0)  # Green wipe
+    #         time.sleep(1)
+    #         self.__colorWipe(self.strip, Color(0, 0, 255), 0)  # Blue wipe
+    #         time.sleep(1)
+    #         self.__colorWipe(self.strip, Color(255, 255, 255), 0)  # Composite White wipe
+    #         time.sleep(1)
+    #         self.__colorWipe(self.strip, Color(255, 255, 255, 255), 0)  # Composite White + White LED wipe
+    #         time.sleep(1)
 
     def red_blinking(self):
         self.__colorWipe(self.strip, Color(255, 0, 0), 0)  # Red wipe
